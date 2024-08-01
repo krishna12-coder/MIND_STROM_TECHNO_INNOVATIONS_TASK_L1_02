e9bd52d6f0f7a4475b5b8fa3618fa94127c248b3
Krishna Farswan <Krishnafarswan597@gmail.com>
2024-08-01 22:14:04 +0530

    socket-chat

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  // console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+
+  socket.on("joinRoom", (room) => socket.join(room));
+
+  socket.on("newMessage", ({ newMessage, room }) => {
+    console.log(room, newMessage);
+    io.in(room).emit("getLatestMessage", newMessage);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket chat BE Started.");
+});
+
+server.listen(8000, () => console.log("App started at port 8000"));
